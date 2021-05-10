c093959f153f08d6a87eb1b8a278aae236fd6714
Ferry Pramudianto <ferryxo@yahoo.com>
2021-05-10 08:45:09 -0400

    change mongo config for docker

---
+++ src/main/java/com/cio/ibm/colorgroup/config/DbConfig.java
@@ -0,0 +1,25 @@
+package com.cio.ibm.colorgroup.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+  /*
+   * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
+   */
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private String port;
+
+    public @Bean
+    MongoClient mongoClient() {
+       return MongoClients.create("mongodb://" + host + ":" + port + "/ibm_cio");
+    }
+}
