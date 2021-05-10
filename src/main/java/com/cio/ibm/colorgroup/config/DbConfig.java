ba9cfeb8ce29ecb49e272929168d88eb567b755f
Ferry Pramudianto <ferryxo@yahoo.com>
2021-05-10 08:45:22 -0400

    change mongo config for docker

--- src/main/java/com/cio/ibm/colorgroup/config/DbConfig.java
+++ src/main/java/com/cio/ibm/colorgroup/config/DbConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class DbConfig {
 
   /*
    * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
--- src/main/java/com/cio/ibm/colorgroup/dao/ColorRepositoryImpl.java
+++ src/main/java/com/cio/ibm/colorgroup/dao/ColorRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.cio.ibm.colorgroup.dao;
 
+import com.cio.ibm.colorgroup.config.DbConfig;
 import com.cio.ibm.colorgroup.models.Membership;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCursor;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
@@ -15,10 +17,13 @@ import java.util.Set;
 
 public class ColorRepositoryImpl implements ColorRepository {
 
+    @Autowired
+    DbConfig dbConfig;
+
     @Override
     public Set<String> findAllColors() {
         Set<String> colors = new HashSet<>();
-        MongoOperations mongoOps = new MongoTemplate(new SimpleMongoClientDbFactory(MongoClients.create(), "ibm_cio"));
+        MongoOperations mongoOps = new MongoTemplate(new SimpleMongoClientDbFactory(dbConfig.mongoClient(), "ibm_cio"));
         MongoCursor<String> result = mongoOps.getCollection("membership").distinct("member.color", String.class).iterator();
 
         while(result.hasNext()) {
@@ -29,7 +34,7 @@ public class ColorRepositoryImpl implements ColorRepository {
 
     @Override
     public String findColorByPerson(String name) {
-        MongoOperations mongoOps = new MongoTemplate(new SimpleMongoClientDbFactory(MongoClients.create(), "ibm_cio"));
+        MongoOperations mongoOps = new MongoTemplate(new SimpleMongoClientDbFactory(dbConfig.mongoClient(), "ibm_cio"));
         Query searchQuery = new Query(Criteria.where("member.name").is(name));
         Membership result = mongoOps.findOne(searchQuery, Membership.class);
         if(result!=null && result.getMember()!=null && !result.getMember().isEmpty()) {
